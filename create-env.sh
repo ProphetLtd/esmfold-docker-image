9188004d5ccb2f7640a1658609b3736976268ab3
ChuNan Liu <bio.chunanliu@gmail.com>
2024-04-09 12:34:24 +0100

    use updated yaml file to install conda  env openfold-venv and py39-esmfold

--- create-env.sh
+++ create-env.sh
@@ -1,33 +1,39 @@
 #!/bin/zsh
 
 # init conda
-conda init zsh
+conda init zsh > /dev/null 2>&1
 
 # source .zshrc to activate conda
 source $HOME/.zshrc
 
 # install openfold
-cd /home/vscode/openfold && \
-    conda activate openfold-venv && \
-    pip install . && \
-    cd /home/vscode && \
-    rm -rf /home/vscode/openfold
-
-# install fair-esm based on openfold conda env
-conda create -n py39-esmfold --clone openfold-venv && \
-    conda activate py39-esmfold && \
-    pip install "fair-esm[esmfold]" && \
-    pip install biotite && \
-    conda clean -a -y && \
-    pip cache purge
+pushd $HOME/openfold
+conda env create -f $HOME/openfold/openfold-venv.yaml \
+    && conda activate openfold-venv \
+    && pip install . \
+    && cd $HOME \
+    && rm -rf $HOME/openfold
+popd
 
 # ------------------- install esmfold command -------------------
+# install esm
+conda create -n py39-esmfold --clone openfold-venv \
+    && conda activate py39-esmfold
+
 # install esm-fold command
-tar -zxvf /home/vscode/esm-main.tar.gz -C /home/vscode && \
-    rm /home/vscode/esm-main.tar.gz && \
-    chown -R vscode:vscode /home/vscode/esm-main && \
-    chmod -R 777 /home/vscode/esm-main && \
-    cd /home/vscode/esm-main && \
-    pip install . && \
-    cd /home/vscode && \
-    rm -rf /home/vscode/esm-main
+pushd $HOME
+tar -zxvf $HOME/esm-main.tar.gz -C $HOME \
+    && rm $HOME/esm-main.tar.gz \
+    && chown -R $USER:$USER $HOME/esm-main \
+    && chmod -R 777 $HOME/esm-main \
+    && pushd $HOME/esm-main \
+    && conda env update -f $HOME/esm-main/py39-esmfold.yaml \
+    && pip install . \
+    && popd \
+    && rm -rf $HOME/esm-main
+popd
+
+# clean up
+conda deactivate \
+    && conda clean -a -y \
+    && pip cache purge
