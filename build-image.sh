7e7d2e24c8b4da66e8c26552444c6192c59c815a
ChuNan Liu <bio.chunanliu@gmail.com>
2024-05-05 03:41:51 +0100

    build image script

--- build-image.sh
+++ build-image.sh
@@ -1,6 +1,26 @@
 #!/bin/zsh
 
-WD=$(pwd)
+BASE=$(dirname $(realpath $0))
+REPONAME=esmfold-docker-image
 
-# Build the image
-docker build -t $USER/esmfold:base .
+# assert CWD basename is $REPONAME
+if [[ $(basename $BASE) != $REPONAME ]]; then
+    echo "Please run this script from the root of the repository."
+    exit 1
+fi
+
+# ------------------------------------------------------------------------------
+# Uncomment any of the following lines to
+# build the image with a specific use case
+# ------------------------------------------------------------------------------
+# # build image, add non-root user
+# docker build --no-cache -t $USER/esmfold:nonroot-devel -f Dockerfiles/Dockerfile.nonroot .
+
+# # build runtime image, add non-root user
+# docker build --no-cache -t $USER/esmfold:nonroot-runtime -f Dockerfiles/Dockerfile.nonroot.runtime .
+
+# build image, root user only
+docker build --no-cache -t $USER/esmfold:root-devel -f Dockerfiles/Dockerfile.root .
+
+# # build runtime image, root user only
+# docker build --no-cache -t $USER/esmfold:root-runtime -f Dockerfiles/Dockerfile.root.runtime .
