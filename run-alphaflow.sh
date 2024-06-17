6d055f34ad01023add076c218494362342ecadaa
JJ Ben-Joseph <jj@tensorspace.ai>
2024-06-17 11:18:39 +0300

    Add initial AlphaFlow container

---
+++ run-alphaflow.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <predict|train> <additional parameters>"
+  exit 1
+fi
+
+# activate py39-esmfold
+conda activate py39-esmfold
+
+command=$1
+
+# Shift the parameters so that $@ contains only the additional parameters
+shift
+
+if [[ "$command" == "predict" ]]; then
+  python /home/vscode/alphaflow/predict.py "$@"
+elif [[ "$command" == "train" ]]; then
+  python /home/vscode/alphaflow/train.py "$@"
+else
+  echo "Unknown command: $command"
+  echo "Usage: $0 <predict|train> <additional parameters>"
+  exit 1
+fi
